36a27c787afebca2af46618487d94a5bc29a614c
Vadim Shvedov <shvedovva@gmail.com>
2025-07-23 12:56:11 +0300

    add .gitignore

---
+++ src/main/java/com/example/util/Program.java
@@ -0,0 +1,31 @@
+package com.example.util;
+
+import com.example.dao.CurrencyDao;
+import com.example.model.Currency;
+
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class Program {
+    public static void main(String[] args) throws SQLException {
+        //DatabaseManager dbm = new DatabaseManager();
+
+        //DatabaseManager.getConnection();
+        //dbm.initDatabase();
+
+        CurrencyDao dao = new CurrencyDao();
+
+        /*System.out.println(dao.getAllCurrencies());
+
+        Optional<Currency> RUB = dao.getCurrencyByCode("RUB");
+
+        System.out.println(RUB);*/
+
+
+        Currency currency = new Currency("TST", "Test Currency", "T");
+        dao.addCurrency(currency);
+
+        System.out.println(dao.getCurrencyById(4));
+
+    }
+}
