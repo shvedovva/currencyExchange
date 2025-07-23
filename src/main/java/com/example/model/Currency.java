225dce4daa2cd5e45557403fda43075dd6dacf45
Vadim Shvedov <shvedovva@gmail.com>
2025-07-23 05:03:52 +0300

    init commit and add model.

---
+++ src/main/java/com/example/model/Currency.java
@@ -0,0 +1,64 @@
+package com.example.model;
+
+import java.util.Objects;
+
+public class Currency {
+    private int id;
+    private String code;
+    private String fullName;
+    private String sign;
+
+    public Currency() {
+    }
+
+    public Currency(int id, String code, String fullName, String sign) {
+        this.id = id;
+        this.code = code;
+        this.fullName = fullName;
+        this.sign = sign;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Currency)) return false;
+        Currency currency = (Currency) o;
+        return id == currency.id && Objects.equals(code, currency.code) && Objects.equals(fullName, currency.fullName) && Objects.equals(sign, currency.sign);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, code, fullName, sign);
+    }
+}
